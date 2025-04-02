8c7f293b5573adb336ab9d921d4090801cd91a84
Shristi-17 <shristi.chauhan2020@gmail.com>
2025-04-02 13:35:02 +0530

    h5 model modified

---
+++ train.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(64,64,1)),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Save the model
+model.save("drowsiness_model.h5")
